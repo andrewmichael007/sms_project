5c1a2a4722df5b57d5ffb7324f875c9a37fc45cd
Andy-Techy <adjeimitchel03@gmail.com>
2025-06-07 11:49:02 +0000

    added auth system file

--- includes/authentication.php
+++ includes/authentication.php
@@ -1,7 +1,12 @@
- 
+
+<!-- sessions are used to track user logins and functions on the website -->
 <?php
+//start the session
 session_start();
 
+//checking if user is logged in and directing them to their seesion
+
+//if user is not set, go to header else...
 if (!isset($_SESSION['user'])) {
     header('Location: ../login.php');
     exit();
